5c3b0728dd8d36e33108664b90d3114e65d91769
Flo700 <florian.martin700@gmail.com>
2023-06-19 14:37:08 +0200

    Modif login

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -9,49 +9,47 @@ function showPassword() {
   }
 }
 
-// Gestion de l'événement du bouton de connexion
-document.getElementById('btnConnexion').addEventListener('click', function(event) {
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+  const showPasswordCheckbox = document.getElementById('showMdp');
+  showPasswordCheckbox.addEventListener('change', showPassword);
 
-  // Récupération des valeurs des champs email et password
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const loginForm = document.getElementById('login-form');
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  // Création de l'objet contenant les données de connexion
-  const loginData = {
-    email: email,
-    password: password
-  };
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-  fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(loginData)
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Vérification de la réponse de l'API
-    if (data.userId && data.token) {
-      const token = data.token;
+    const loginData = {
+      email: email,
+      password: password
+    };
 
-      // Stockage du token dans le localStorage
-      localStorage.setItem('token', token);
-
-      // Redirection vers la page d'accueil
-      window.location.href = './index.html';
-    } else {
-      const errorContainer = document.getElementById('error-container');
-      const error = document.createElement('p');
-		  error.innerText = 'email ou mot de passe incorrect';
-		  error.style.textAlign = 'center';
-		  error.style.color = 'red';
-		  error.style.marginBottom = '35px';
-      errorContainer.appendChild(error)
-    }
-  })
-  .catch(error => {
-    console.error('Erreur lors de la requête de login:', error);
+    fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.userId && data.token) {
+          const token = data.token;
+          localStorage.setItem('token', token);
+          window.location.href = './index.html';
+        } else {
+          const errorContainer = document.getElementById('error-container');
+          const error = document.createElement('p');
+          error.innerText = 'Email ou mot de passe incorrect';
+          error.style.textAlign = 'center';
+          error.style.color = 'red';
+          error.style.marginBottom = '35px';
+          errorContainer.appendChild(error);
+        }
+      })
+      .catch(error => {
+        console.error('Erreur lors de la requête de login:', error);
+      });
   });
 });
